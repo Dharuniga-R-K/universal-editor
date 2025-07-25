7d6f5dda7e1babcf750ff9ecf6e182631b43e698
Dharuniga R K <Dharuniga.R.K@gds.ey.com>
2025-07-25 17:31:39 +0530

    modified nav

--- blocks/navigation/navigation.js
+++ blocks/navigation/navigation.js
@@ -1,14 +1,24 @@
-<nav class="nav-block">
-  {items.map(item => (
-    <div class="nav-item">
-      <a href={item.link}>{item.title}</a>
-      {item.children && (
-        <div class="dropdown">
-          {item.children.map(child => (
-            <a href={child.link}>{child.title}</a>
-          ))}
-        </div>
-      )}
-    </div>
-  ))}
-</nav>
+export default function renderNavigationBlock(container) {
+    const html = `
+      <nav class="nav-block">
+        <ul class="nav-items">
+          <li class="nav-item">Platform
+            <ul class="dropdown">
+              <li>Web SDK</li>
+              <li>React SDK</li>
+              <li>Java SDK</li>
+            </ul>
+          </li>
+          <li class="nav-item">Resources
+            <ul class="dropdown">
+              <li>Docs</li>
+              <li>Tutorial</li>
+              <li>Community</li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    `;
+    container.innerHTML = html;
+  }
+  
