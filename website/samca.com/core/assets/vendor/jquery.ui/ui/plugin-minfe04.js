452c69cb93c430e55d75061bc498486217fa360a
Dharuniga R K <Dharuniga.R.K@gds.ey.com>
2025-07-28 13:24:23 +0530

    removed website

--- website/samca.com/core/assets/vendor/jquery.ui/ui/plugin-minfe04.js
+++
@@ -1,4 +0,0 @@
-!(function (e) {
-  typeof define === 'function' && define.amd ? define(['jquery', './version'], e) : e(jQuery);
-}(((e) => e.ui.plugin = { add(n, i, t) { let u; const o = e.ui[n].prototype; for (u in t)o.plugins[u] = o.plugins[u] || [], o.plugins[u].push([i, t[u]]); }, call(e, n, i, t) { let u; const o = e.plugins[n]; if (o && (t || e.element[0].parentNode && e.element[0].parentNode.nodeType !== 11)) for (u = 0; u < o.length; u++)e.options[o[u][0]] && o[u][1].apply(e.element, i); } })));
-// # sourceMappingURL=plugin-min.js.map
--- website/samca.com/core/assets/vendor/jquery.ui/ui/version-minfe04.js
+++
@@ -1,4 +0,0 @@
-!(function (e) {
-  typeof define === 'function' && define.amd ? define(['jquery'], e) : e(jQuery);
-}(((e) => (e.ui = e.ui || {}, e.ui.version = '1.14.1'))));
-// # sourceMappingURL=version-min.js.map
